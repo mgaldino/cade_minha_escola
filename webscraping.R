ae22b06365500abbe491cb89e5dca09f4eb831f1
mgaldino <mcz.fea@gmail.com>
2016-06-17 18:04:02 -0300

    Criei função para pegar dados da página de licitação. Corrigi alguns dados.

--- webscraping.R
+++ webscraping.R
@@ -1,3 +1,129 @@
 ## webscrapping
 
+## primeiro, pegando lista de obras
 
+setwd("/Users/natalia/Documents/Manoel")
+
+# arquivo com lista de obras
+escolas <- read.table("Escolas_Brasil_v3.csv", header=T, as.is=T, sep = ",",  quote="\"", na.strings = "NA",
+                      fileEncoding = "UTF-8") 
+# coletando id das obras
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+escolas <- escolas %>%
+  mutate(id = str_extract(Nome_Obra, "\\([0-9]+\\)"),
+         id = gsub("\\(", "", id),
+         id = gsub("\\)", "", id))
+  
+head(escolas)
+
+library(RCurl)
+library(XML)
+setInternet2(TRUE)
+options(timeout=500)
+
+getConstructionData <- function(url) {
+  html <- getURL(url)
+  doc <- htmlParse(html)   # parseia url
+  linksAux <- xpathSApply(doc, "//dt", xmlValue)   # coleta os links
+  linksAux1 <- xpathSApply(doc, "//dd", xmlValue) 
+  df <- data.frame(linksAux, linksAux1)
+}
+
+url <- "http://simec.mec.gov.br/painelObras/contratacao.php?obra="
+url_final <- paste(url, escolas$id, sep="")
+
+lista_df <- list()
+n <- length(url_final)
+for ( i in 1:n) {
+  lista_df[[i]] <- getConstructionData(url_final[i])
+  Sys.sleep(.5) # para não derrubar o site
+  if(i %% 1000 == 0) print(i)
+}
+
+
+length(lista_df)
+i <- 1
+while ( i <= n) {
+  if(i %in% c(6161, 9178, 11296)) { # checa se são ids que a busca não retornou escolas
+    i <- i+1
+  }
+  lista_df[[i]]$id <- escolas$id[i] # adiciona id das escolas
+  aux <- which(duplicated(lista_df[[i]]$linksAux)) # as vezes tem meúltiplas linhas iguais, torna elas diferentes
+  lista_df[[i]]$linksAux <- as.character( lista_df[[i]]$linksAux)
+  lista_df[[i]]$linksAux[aux] <- paste(lista_df[[i]]$linksAux[aux], 1:length(aux), sep="")
+  i <- i+1
+}
+
+## "Data de Término do Contrato:"
+"Prazo de Vigência:"
+
+obras <- bind_rows(lista_df)
+dim(obras)
+head(obras, 30)
+
+obras$linksAux[obras$id==1365 & (is.na(obras$linksAux) | grepl("NA", obras$linksAux ))] <-
+  c("Prazo de Vigência1:", "Data de Término do Contrato1:", "Total da Planilha Contratada1:" )
+
+
+obras1 <- obras %>%
+  spread(linksAux, linksAux1)
+which(grepl("Município", names(obras1)))
+
+vec_colunas_obras <- c(which(grepl("Município", names(obras1))),
+                 which(grepl("Endereço", names(obras1))),
+                 which(grepl("Termo/Convênio", names(obras1))))
+
+
+base_obras <- obras1 %>%
+  select(c(1:4, vec_colunas_obras))
+
+base_obras_status <- obras1 %>%
+  select(which(grepl("Situação", names(obras1))))     
+
+vec_colunas_empresas <- c(which(grepl("Empresa Contratada", names(obras1))),
+                       which(grepl("Endereço", names(obras1))),
+                       which(grepl("Termo/Convênio", names(obras1))))
+
+base_empresas <- obras1 %>%
+  select(which(grepl("Situação", names(obras1))))         
+
+names(base_obras)[2] <- "area_construida_m2"
+names(base_obras) <- gsub(":", "", names(base_obras))
+base_obras$area_construida_m2 <- gsub(" m2", "", base_obras$area_construida_m2)
+base_obras$area_construida_m2_1 <- as.numeric(base_obras$area_construida_m2)
+head(base_obras)
+dim(obras1)
+names(obras1)
+
+url1 <- "http://simec.mec.gov.br/painelObras/contratacao.php?obra=1365"
+teste <- getURL(url1)
+pegaLinks(url_final, "#")
+pegaLinks(url_final, "Edital")
+
+getConstructionData <- function(url) {
+  html <- getURL(url)
+  doc <- htmlParse(html)   # parseia url
+  linksAux <- xpathSApply(doc, "//dt", xmlValue)   # coleta os links
+  linksAux1 <- xpathSApply(doc, "//dd", xmlValue) 
+  df <- data.frame(linksAux, linksAux1)
+}
+
+x <- getConstructionData(url_final)
+View(x)
+
+linksFinais <- unique(linksAux[grep(padrao.inicial, linksAux)]) # me traz apenas os links certos
+free(doc)
+
+
+library(RCurl)
+library(XML)
+txt <- htmlToText(url1)
+txt
+http://simec.mec.gov.br/painelObras/licitacao.php?obra=1381#HOMOLOGA%C3%87%C3%83O%20DA%20LICITA%C3%87%C3%83O%201
+
+http://simec.mec.gov.br/painelObras/dadosobra.php?obra=1381
+
+http://simec.mec.gov.br/painelObras/licitacao.php?obra=1381#Edital%20PROINF%C3%82NCIA
